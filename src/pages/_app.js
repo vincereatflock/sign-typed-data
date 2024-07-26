e8174da828be8c651548afcd7b4e4c7dd04976a3
Alfred Crosby <alfred@vincere.dev>
2024-07-26 18:58:01 +0700

    debug sign typed data

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,26 @@
+import { BaseSepolia, EthereumSepolia } from "@particle-network/chains";
+import { ModalProvider } from "@particle-network/connectkit";
+import "@particle-network/connectkit/dist/index.css";
+import { evmWallets, solanaWallets } from "@particle-network/connectors";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ModalProvider
+      options={{
+        projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+        clientKey: process.env.NEXT_PUBLIC_CLIENT_KEY,
+        appId: process.env.NEXT_PUBLIC_APP_ID,
+        chains: [BaseSepolia, EthereumSepolia],
+        connectors: [...evmWallets({ projectId: "21d2a01621c47fb5f34b06c6390ac0bb", showQrModal: true }), ...solanaWallets()],
+        wallet: {
+          customStyle: {
+            supportChains: [BaseSepolia, EthereumSepolia],
+          },
+        },
+      }}
+    >
+      <Component {...pageProps} />
+    </ModalProvider>
+  );
 }
